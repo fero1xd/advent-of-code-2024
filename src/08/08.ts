f99609eded8cb8c1e5de753a27dea868bcbd0679
Pranjal Butola <pranjalbutola12@gmail.com>
2024-12-08 22:36:17 +0530

    day8 part2

--- src/08/08.ts
+++ src/08/08.ts
@@ -75,6 +75,64 @@ const project_antinode = (map: Map, pair: Position[]): Position[] | null => {
   return antinodes;
 };
 
+const is_out_of_bounds = (pos: Position, map: Map) => {
+  return (
+    pos.x < 0 || pos.x > map.width - 1 || pos.y < 0 || pos.y > map.height - 1
+  );
+};
+
+const project_antinode_p2 = (map: Map, pair: Position[]): Position[] | null => {
+  const a1 = pair[0];
+  const a2 = pair[1];
+
+  const antinodes: Position[] = [];
+  let i = 0;
+
+  for (const antenna of pair) {
+    antinodes.push(antenna);
+
+    let other = i++ === 0 ? a2 : a1;
+    const dir_vec: Position = {
+      x: other.x - antenna.x,
+      y: other.y - antenna.y,
+    };
+    const antinode_pos_vec: Position = { x: -dir_vec.x, y: -dir_vec.y };
+    const antinode_pos: Position = _add_position(antenna, antinode_pos_vec);
+
+    if (is_out_of_bounds(antinode_pos, map)) {
+      // Out of bounds
+      continue;
+    }
+    antinodes.push(antinode_pos);
+
+    other = antenna;
+    let new_antenna = antinode_pos;
+
+    while (true) {
+      const dir_vec: Position = {
+        x: other.x - new_antenna.x,
+        y: other.y - new_antenna.y,
+      };
+      const antinode_pos_vec: Position = { x: -dir_vec.x, y: -dir_vec.y };
+      const antinode_pos: Position = _add_position(
+        new_antenna,
+        antinode_pos_vec
+      );
+
+      if (is_out_of_bounds(antinode_pos, map)) {
+        // Out of bounds
+        break;
+      }
+
+      antinodes.push(antinode_pos);
+      other = new_antenna;
+      new_antenna = antinode_pos;
+    }
+  }
+
+  return antinodes;
+};
+
 const create_antinodes = (map: Map): Position[] => {
   const antinodes = new Set<string>();
 
@@ -87,7 +145,7 @@ const create_antinodes = (map: Map): Position[] => {
     for (let i = 0; i < positions.length; i++) {
       for (let j = i + 1; j < positions.length; j++) {
         const pair = [positions[i], positions[j]];
-        const antinode_pos = project_antinode(map, pair);
+        const antinode_pos = project_antinode_p2(map, pair);
         if (antinode_pos != null) {
           for (const apos of antinode_pos) {
             antinodes.add(JSON.stringify(apos));
