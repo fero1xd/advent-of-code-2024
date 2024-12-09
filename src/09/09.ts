ff239b049e0bb89f8ee00b13b78ef3f746634dcf
Pranjal Butola <pranjalbutola12@gmail.com>
2024-12-09 17:12:47 +0530

    day 9 part 2

--- src/09/09.ts
+++ src/09/09.ts
@@ -5,7 +5,7 @@ const generate_blocks = (disk_map: string[]): string[] => {
   let file_index = 0;
 
   for (let i = 0; i < disk_map.length; i++) {
-    const is_free_space = Number(disk_map[i]) % 2 === 0;
+    const is_free_space = i % 2 !== 0;
 
     for (let j = 0; j < Number(disk_map[i]); j++) {
       if (!is_free_space) {
@@ -15,10 +15,10 @@ const generate_blocks = (disk_map: string[]): string[] => {
       }
     }
 
-    !is_free_space && file_index++;
+    if (!is_free_space) {
+      file_index++;
+    }
   }
-
-  console.log(file_index);
 
   return blocks;
 };
@@ -44,15 +44,73 @@ const rearrange_blocks = (blocks: string[]) => {
   }
 };
 
+const set_free_block = (
+  blocks: string[],
+  starting: number,
+  len: number,
+  file: string
+) => {
+  for (let i = starting; i < starting + len; i++) {
+    blocks[i] = file;
+  }
+};
+
+const rearrange_blocks_p2 = (blocks: string[]) => {
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    if (isNaN(Number(blocks[i]))) {
+      continue;
+    }
+
+    let file_length = 0;
+    let j = i;
+    while (j >= 0 && blocks[j] === blocks[i]) {
+      file_length++;
+      j--;
+    }
+
+    for (let k = 0; k <= j; k++) {
+      if (blocks[k] !== '.') continue;
+
+      let dots_len = 0;
+      // Look for file_length dots
+      for (let x = k; x <= j; x++) {
+        if (blocks[x] === '.') dots_len++;
+        else break;
+      }
+
+      if (dots_len >= file_length) {
+        set_free_block(blocks, k, file_length, blocks[i]);
+        const st = i - file_length;
+        set_free_block(blocks, st + 1, file_length, '.');
+        break;
+      }
+    }
+
+    // Have to +1 coz loop is gonna do the -1
+    i = j + 1;
+  }
+};
+
+const calculate_checksum = (blocks: string[]) => {
+  let total = 0;
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i] === '.') continue;
+
+    total += i * Number(blocks[i]);
+  }
+
+  return total;
+};
+
 const main = async () => {
-  const text = await Deno.readTextFile('./src/09/sample.txt');
+  const text = await Deno.readTextFile('./src/09/input.txt');
   const disk_map = text.split('');
 
   const blocks = generate_blocks(disk_map);
+  rearrange_blocks_p2(blocks);
 
-  console.log({ before: blocks });
-  rearrange_blocks(blocks);
-  console.log(blocks);
+  console.log({ checksum: calculate_checksum(blocks) });
 };
 
 main();
